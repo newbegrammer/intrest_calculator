667cc11f2c5251bee2de5dd9a1f6549afcef464a
Ayush Tripathi <88437641+newbegrammer@users.noreply.github.com>
2022-02-22 14:27:52 +0530

    Update intrest calculator.py

--- intrest calculator.py
+++ intrest calculator.py
@@ -1,11 +1,11 @@
-prin = int(input("Enter the principle money: "))
+principle = int(input("Enter the principle money: "))
 rate = int(input("Enter the rate of intrest: "))
 time = int(input("Enter the time in years: "))
 r = rate/100
 ti = time + 2
 t = time+1
-si = prin*r
-amount = prin+si
+si = principle*r
+amount = principle+si
 while(True):
 
     t = t-1
@@ -14,7 +14,7 @@ while(True):
         break
     if(t>1):
         amount = amount + amount*r
-        print("your principle money",prin," after",ti-t,"years:", amount)
+        print("your principle money",principle," after",ti-t,"years:", amount)
         continue
 
-print("total intrest = ", amount-prin)
+print("total intrest = ", amount-principle)
